9486aef243d35e58fa6528238fcc51b8dd945691
pmatys3914 <piotr.matys.3914@gmail.com>
2022-03-27 23:57:35 +0200

    PPZO 1

---
+++ ppzo/1/Program.cs
@@ -0,0 +1,44 @@
+﻿namespace Tea
+{
+    class Program
+    {
+        public static string pathInput = "tea-data.txt";
+        public static string pathOutput = "result-1.txt";
+        static void Main(string[] args)
+        {
+            // Check if the input file exists, return otherwise
+            if (!File.Exists(pathInput))
+            {
+                Console.Write("File missing!");
+                return;
+            }
+            // Create a list for the records
+            List<string> list = new List<string>();
+            // Read the list with a stream reader
+            using (StreamReader sr = File.OpenText(pathInput))
+            {
+                string? s;
+                while((s = sr.ReadLine()) != null)
+                {
+                    // If the line is not empty and doesn't begin with #, add it to the list
+                    if(s != "" && s[0] != '#')
+                    {
+                        list.Add(s);
+                    }
+                }
+            }
+
+            // Reverse the list
+            list.Reverse();
+
+            // Create the output file and dump the list into it
+            using (StreamWriter sw = File.CreateText(pathOutput))
+            {
+                foreach( string s in list)
+                {
+                    sw.WriteLine(s);
+                }
+            }
+        }
+    }
+}
